8e303c37eccf96a6508ab8b30b50fce576d5121c
nthnua <97590341+nthnua@users.noreply.github.com>
2022-03-07 18:13:56 +0530

    Add db connection setup

---
+++ src/util/db.ts
@@ -0,0 +1,30 @@
+import { Db, MongoClient } from 'mongodb'
+
+let _db: Db
+const url = process.env.MONGO_URL
+
+const mongoConnect = async (): Promise<void> => {
+  if (url !== undefined) {
+    try {
+      const client = await MongoClient.connect(url)
+      _db = client.db()
+      console.log('DB connected')
+    } catch (e) {
+      console.error(e)
+    }
+  } else {
+    throw new Error('DB connection string not specified!')
+  }
+}
+
+const getDB = (): Db => {
+  if (_db != null) {
+    return _db
+  }
+  throw new Error('DB not found!')
+}
+
+export {
+  mongoConnect,
+  getDB
+}
