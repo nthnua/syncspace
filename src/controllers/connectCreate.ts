585df915308647a66dcaafecca09794e9e526d15
nthnua <97590341+nthnua@users.noreply.github.com>
2022-04-08 19:15:47 +0530

    Implement space creation

---
+++ src/controllers/connectCreate.ts
@@ -0,0 +1,19 @@
+import { hash } from "bcrypt";
+import { RequestHandler } from "express";
+import Space from "../model/space";
+
+const createNewSpace: RequestHandler = (req, res, next) => {
+  const name = req.body.name
+  const key = req.body.key
+  hash(name + key, 10).then(hash => {
+    new Space(name, hash).save().then(r => {
+      res.status(201).json({
+        message: 'space created succesfully'
+      })
+    }).catch(err => console.error(err))
+  }).catch(err => console.error(err))
+}
+
+export {
+  createNewSpace
+}
